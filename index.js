762df601e14ea181bbff1fc2e8a30bacd19bbc12
Matheus Rossini <matheus_rossini2008@hotmail.com>
2022-03-12 12:26:47 -0300

    Motamos uma URL

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-https://api.openweathermap.org/data/2.5/forecast?q=Itu&appid=APPID
+require('dotenv').config()
 
+// const PROTOCOL = process.env.PROTOCOL
+// const BASE_URL = process.env.BASE_URL
+// const APPID = process.env.APPID
+// const LANGUAGE = process.env.LANGUAGE
+// const UNITS = process.env.UNITS
+
+const {PROTOCOL, BASE_URL, APPID, LANGUAGE, UNITS, Q} = process.env
+
+
+
+const url = `${PROTOCOL}://${BASE_URL}?appid=${APPID}&lang=${LANGUAGE}&units=${UNITS}&q=${Q}`
+
+
+console.log(url)
